d613fc4a1c7fd8e2666f229a8faed0ee09d91310
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-26 15:21:21 +0500

    5

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import './css/HomePage.css'
+import './css/Home.css'
 
 const HomePage = () => {
   return (
-    <div className="home-page">
-      <h1>Welcome to the Home Page</h1>
+    <div className="home">
+      <h1 className="head">Welcome to the Home Page</h1>
       <p>This is where you can see your dashboard or profile information</p>
     </div>
   );
