4b793a9fb9c187092cc6ce7c0ac4ba418666d121
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-26 23:18:46 +0500

    h

---
+++ src/components/pages/MangerUsersMenu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const ManageUsersMenu = () => {
+  return (
+    <div>
+      <h2>Manage Users</h2>
+      <p>User management information</p>
+    </div>
+  );
+};
+
+export default ManageUsersMenu;
