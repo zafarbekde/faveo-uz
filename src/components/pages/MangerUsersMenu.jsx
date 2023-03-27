63619cdb875f3e824f6aab8bd4ac99946a1e049e
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-27 22:01:22 +0500

    de

--- src/components/pages/MangerUsersMenu.jsx
+++ src/components/pages/MangerUsersMenu.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import UserTable from "../pages/UserTable";
+import EditUserForm from "../pages/EditUserForm";
+import AddUserForm from "../pages/AddUserForm";
 
 const MangerUsersMenu = () => {
   const [users, setUsers] = useState([]);
