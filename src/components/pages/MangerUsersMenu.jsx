70e675b7cb956102b72dadf6f419d8a4e7195a66
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-27 20:52:10 +0500

    dd

--- src/components/pages/MangerUsersMenu.jsx
+++ src/components/pages/MangerUsersMenu.jsx
@@ -1,10 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import UserTable from "../pages/UserTable";
 
 const ManageUsersMenu = () => {
+  const initialFormState = { id: null, name: "", username: "", email: "" };
+
+  const [users, setUsers] = useState([]);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+  const [editing, setEditing] = useState(false);
+
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+  };
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+  };
+
+  const editRow = (user) => {
+    setEditing(true);
+    setCurrentUser({ id: user.id, name: user.name, username: user.username, email: user.email });
+  };
+
   return (
     <div>
       <h2>Manage Users</h2>
-      <p>User management information</p>
+      <div>
+        <h3>Add User</h3>
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            if (!currentUser.name || !currentUser.username || !currentUser.email) return;
+            if (editing) {
+              updateUser(currentUser.id, currentUser);
+            } else {
+              addUser(currentUser);
+            }
+            setCurrentUser(initialFormState);
+          }}
+        >
+          <label>Name</label>
+          <input type="text" name="name" value={currentUser.name} onChange={(event) => setCurrentUser({ ...currentUser, name: event.target.value })} />
+          <label>Username</label>
+          <input type="text" name="username" value={currentUser.username} onChange={(event) => setCurrentUser({ ...currentUser, username: event.target.value })} />
+          <label>Email</label>
+          <input type="email" name="email" value={currentUser.email} onChange={(event) => setCurrentUser({ ...currentUser, email: event.target.value })} />
+          <button type="submit">{editing ? "Update" : "Add"}</button>
+        </form>
+      </div>
+      <div>
+        <h3>User List</h3>
+        <UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+        </div>
     </div>
   );
 };
