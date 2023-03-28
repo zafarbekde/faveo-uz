ecbf878f60146c46029495300c7780bf2fd0fcd7
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-03-28 12:53:20 +0500

    d

--- src/components/pages/EditUserModal.jsx
+++ src/components/pages/EditUserModal.jsx
@@ -1,94 +1,91 @@
 import React, { useState } from "react";
 
-const EditUserModal = ({ show, user, onClose, onSave }) => {
-  const [name, setName] = useState(user.name);
-  const [lastName, setLastName] = useState(user.lastName);
-  const [email, setEmail] = useState(user.email);
-  const [birthday, setBirthday] = useState(user.birthday);
-  const [phone, setPhone] = useState(user.phone);
+const EditUserModal = ({ user, updateUser, closeModal }) => {
+  const [userData, setUserData] = useState(user);
 
-  const handleSave = () => {
-    onSave({
-      id: user.id,
-      name,
-      lastName,
-      email,
-      birthday,
-      phone,
-    });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserData({ ...userData, [name]: value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    updateUser(userData);
+    closeModal();
   };
 
   return (
-    <div className={`modal ${show ? "show" : ""}`}>
-      <div className="modal-dialog">
-        <div className="modal-content">
-          <div className="modal-header">
-            <h5 className="modal-title">Edit User</h5>
-            <button type="button" className="close" onClick={onClose}>
-              <span aria-hidden="true">&times;</span>
+    <div className="modal">
+      <div className="modal-content">
+        <h2>Edit User</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="id">ID:</label>
+            <input
+              type="text"
+              id="id"
+              name="id"
+              value={userData.id}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={userData.name}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="surname">Surname:</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              value={userData.surname}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={userData.email}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="birthday">Birthday:</label>
+            <input
+              type="date"
+              id="birthday"
+              name="birthday"
+              value={userData.birthday}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div>
+            <label htmlFor="phones">Phone:</label>
+            <input
+              type="text"
+              id="phones"
+              name="phones"
+              value={userData.phones}
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="modal-buttons">
+            <button type="submit">Save</button>
+            <button type="button" onClick={closeModal}>
+              Cancel
             </button>
           </div>
-          <div className="modal-body">
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                className="form-control"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="lastName">Last Name:</label>
-              <input
-                type="text"
-                id="lastName"
-                className="form-control"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="email">Email:</label>
-              <input
-                type="email"
-                id="email"
-                className="form-control"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="birthday">Birthday:</label>
-              <input
-                type="date"
-                id="birthday"
-                className="form-control"
-                value={birthday}
-                onChange={(e) => setBirthday(e.target.value)}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="phone">Phone:</label>
-              <input
-                type="tel"
-                id="phone"
-                className="form-control"
-                value={phone}
-                onChange={(e) => setPhone(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className="modal-footer">
-            <button type="button" className="btn btn-secondary" onClick={onClose}>
-              Close
-            </button>
-            <button type="button" className="btn btn-primary" onClick={handleSave}>
-              Save
-            </button>
-          </div>
-        </div>
+        </form>
       </div>
     </div>
   );
