11ed224b3b3370bdd189dbeefbedb34fe783a5a8
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-04-02 23:59:16 +0500

    g

--- src/components/pages/UserTable.jsx
+++ src/components/pages/UserTable.jsx
@@ -1,33 +1,49 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import EditUserForm from '../pages/EditUserForm';
+import React, { useState } from "react";
+import { createPortal } from "react-dom";
+import EditUserModal from "../pages/EditUserForm";
+import AddUserModal from "../pages/AddUserForm";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPen } from "@fortawesome/free-solid-svg-icons";
+import "../../css/UserTable.css";
 
-const UserTable = ({ users, onEditUser }) => {
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [currentUser, setCurrentUser] = useState(null);
 
-  const handleEditUser = (user) => {
-    setCurrentUser(user);
-    setIsModalOpen(true);
+const UserTable = ({ users, editRow, deleteUser }) => {
+  const [editingUser, setEditingUser] = useState(null);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  
+
+  const openEditModal = (user) => {
+    setEditingUser(user);
+    setShowEditModal(true);
   };
 
-  const handleCloseModal = () => {
-    setIsModalOpen(false);
-    setCurrentUser(null);
+  const closeEditModal = () => {
+    setShowEditModal(false);
+    setEditingUser(null);
+  };
+
+  const openAddModal = () => {
+    setShowAddModal(true);
+  };
+
+  const closeAddModal = () => {
+    setShowAddModal(false);
   };
 
   return (
-    <div>
+    <div className="user-table">
+      <h2>User Table</h2>
       <table>
         <thead>
           <tr>
-            <th>User ID</th>
-            <th>User Name</th>
-            <th>User Surname</th>
-            <th>User Email</th>
-            <th>User Birthday</th>
-            <th>User Phones</th>
-            <th>Action</th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Last Name</th>
+            <th>Email</th>
+            <th>Birthday</th>
+            <th>Phone</th>
+            <th>Edit/Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -35,22 +51,40 @@ const UserTable = ({ users, onEditUser }) => {
             <tr key={user.id}>
               <td>{user.id}</td>
               <td>{user.name}</td>
-              <td>{user.surname}</td>
+              <td>{user.lastName}</td>
               <td>{user.email}</td>
               <td>{user.birthday}</td>
-              <td>{user.phones.join(', ')}</td>
+              <td>{user.phone}</td>
               <td>
-                <button onClick={() => handleEditUser(user)}>Edit</button>
+                <button onClick={() => openEditModal(user)}>
+                  <FontAwesomeIcon icon={faPen} />
+                </button>
+                <button onClick={() => deleteUser(user.id)}>Delete</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      {isModalOpen &&
-        ReactDOM.createPortal(
-          <EditUserForm user={currentUser} onClose={handleCloseModal} onSave={onEditUser} />,
-          document.getElementById('modal-root')
+
+      {showEditModal &&
+        createPortal(
+          <EditUserModal
+            user={editingUser}
+            updateUser={updateUser}
+            closeModal={closeEditModal}
+          />,
+          document.getElementById("modal-root")
         )}
+
+      {showAddModal &&
+        createPortal(
+          <AddUserModal addUser={addUser} closeModal={closeAddModal} />,
+          document.getElementById("modal-root")
+        )}
+
+      <button onClick={openAddModal}>Add User</button>
+
+      <ManagerUserMenu users={users} deleteUser={deleteUser} />
     </div>
   );
 };
