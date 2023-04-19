b34d0ce04b7c11d35b10785301dbc53bb15db18f
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-04-19 15:29:01 +0500

    backend fix it done

--- src/admin/backend/server.js
+++ src/admin/backend/server.js
@@ -1,19 +1,16 @@
 const express = require('express');
-const markdownToJson = require('markdown-to-json');
+const usersData = require('../ManageUsers.json');
 const app = express();
 
 app.get('/api/v1/users', (req, res) => {
-  // Convert Markdown to JSON
-  markdownToJson('./admin/ManageUsers.md', (data) => {
-    // Send the data as a JSON response
-    res.json(data);
-  });
+  // Send the data as a JSON response
+  res.json(usersData);
 });
 
 app.use((req, res) => {
   res.status(404).send('Page not found');
 });
 
-app.listen(3000, () => {
-  console.log('Server running at http://localhost:3000/');
+app.listen(2000, () => {
+  console.log('Server running at http://localhost:2000/');
 });
--- src/components/pages/MangerUsersMenu.jsx
+++ src/components/pages/MangerUsersMenu.jsx
@@ -7,7 +7,7 @@ function ManageUsersForm() {
 
   useEffect(() => {
     // fetch users from backend API and set state
-    axios.get('admin/ManageUsers/api/v1/users')
+    axios.get('http://localhost:2000/api/v1/users')
       .then(response => setUsers(response.data))
       .catch(error => console.error(error));
   }, []);
