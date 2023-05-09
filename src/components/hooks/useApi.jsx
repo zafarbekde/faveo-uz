0dc4544fbe20a75e8bb9262f5ba9bff2980fd375
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-09 17:30:27 +0500

    update useApi.jsx

---
+++ src/components/hooks/useApi.jsx
@@ -0,0 +1,39 @@
+// hooks/useApi.js
+import { useQuery, useMutation, useQueryClient } from 'react-query';
+import axios from 'axios';
+import { useCookies } from 'react-cookie';
+
+const useApi = () => {
+  const [cookies] = useCookies(['accessToken', 'refreshToken']);
+  const queryClient = useQueryClient();
+
+  const axiosInstance = axios.create({
+    baseURL: 'your-api-url',
+    headers: {
+      Authorization: `Bearer ${cookies.accessToken}`,
+    },
+  });
+
+  const fetchData = async (key, url) => {
+    const response = await axiosInstance.get(url);
+    if (response.status !== 200) {
+      throw new Error('Error fetching data');
+    }
+    return response.data;
+  };
+
+  const postData = async ({ url, data }) => {
+    const response = await axiosInstance.post(url, data);
+    if (response.status !== 201) {
+      throw new Error('Error posting data');
+    }
+    return response.data;
+  };
+
+  const useFetch = (url, options) => useQuery(['data', url], () => fetchData('data', url), options);
+  const usePost = (options) => useMutation(postData, options);
+
+  return { useFetch, usePost, queryClient };
+};
+
+export default useApi;
