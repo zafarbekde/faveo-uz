eea991269d6d04f2eefe8e09ce3410286f5b55f8
Zafarbek <113168777+zafarbekde@users.noreply.github.com>
2023-05-09 17:29:02 +0500

    update useAuth.jsx

---
+++ src/components/hooks/useAuth.jsx
@@ -0,0 +1,39 @@
+// hooks/useAuth.js
+import { useState, useEffect } from 'react';
+import { useCookies } from 'react-cookie';
+
+const useAuth = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(['accessToken', 'refreshToken']);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (cookies.accessToken && cookies.refreshToken) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+  }, [cookies]);
+
+  const login = async (email, password) => {
+    const response = await axios.post('https:localhost:8080/api/v1/auth/login', {
+      email,
+      password,
+    });
+
+    if (response.data.accessToken && response.data.refreshToken) {
+      setCookie('accessToken', response.data.accessToken, { path: '/' });
+      setCookie('refreshToken', response.data.refreshToken, { path: '/' });
+      setIsAuthenticated(true);
+    }
+  };
+
+  const logout = () => {
+    removeCookie('accessToken', { path: '/' });
+    removeCookie('refreshToken', { path: '/' });
+    setIsAuthenticated(false);
+  };
+
+  return { isAuthenticated, login, logout };
+};
+
+export default useAuth;
